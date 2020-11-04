9feb9ba8b04a00bac9329e82ab5802bb9b75a329
08fdange <08fdange@gmail.com>
2020-11-04 15:04:08 -0500

    Added category to Item model

--- backend/db/schema.rb
+++ backend/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_11_04_075852) do
+ActiveRecord::Schema.define(version: 2020_11_04_200215) do
 
   create_table "items", force: :cascade do |t|
     t.string "img_url"
@@ -25,6 +25,7 @@ ActiveRecord::Schema.define(version: 2020_11_04_075852) do
     t.string "location"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "category"
   end
 
   create_table "users", force: :cascade do |t|
