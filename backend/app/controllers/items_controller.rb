67af036f0af0bddf9c84be8968f49af8e27ece0b
08fdange <08fdange@gmail.com>
2020-11-29 16:49:48 -0500

    Updated item controller with working picture upload and attachment to item

--- backend/app/controllers/items_controller.rb
+++ backend/app/controllers/items_controller.rb
@@ -1,42 +1,56 @@
 class ItemsController < ApplicationController
-  # before_action :authenticate_request
+  before_action :authenticate_request, only: [:create, :update, :destroy]
   before_action :set_item, only: [:show, :update, :destroy]
 
   # GET /items
   def index
     @items = Item.all
-
-    render json: @items
+        
+    items_json = ItemSerializer.new(@items).serialized_json
+    render json: items_json, status: :ok
+    
   end
 
-  # GET /items/1
+  # GET users/:username/items/:id
   def show
-    render json: @item
+    render json: ItemSerializer.new(@item).serialized_json, status: :ok
   end
 
   # POST /items
   def create
     @item = Item.new(item_params)
+    @item.user_id = User.find_by(username: params[:username]).id
 
     if @item.save
-      render json: @item, status: :created, location: @item
+      render json: ItemSerializer.new(@item), status: :created
     else
       render json: @item.errors, status: :unprocessable_entity
     end
   end
 
-  # PATCH/PUT /items/1
+  # PATCH/PUT users/:username/items/:id
   def update
     if @item.update(item_params)
-      render json: @item
+      render json: ItemSerializer.new(@item), status: :ok
     else
       render json: @item.errors, status: :unprocessable_entity
     end
   end
 
-  # DELETE /items/1
+  # DELETE users/:username/items/:id
   def destroy
     @item.destroy
+  end
+
+  def upload_picture
+    @item = Item.find_by(id: picture_params[:id])
+    @item.picture.attach(picture_params[:picture])
+    if @item.picture.attached?
+        picture_serializer = PictureSerializer.new(picture: @item.picture, item: @item)
+        render json: picture_serializer.serialize_new_picture()
+    else
+        render json: {errors: "No Picture Attached"}, status: 400
+    end
   end
 
   private
@@ -47,6 +61,11 @@ class ItemsController < ApplicationController
 
     # Only allow a trusted parameter "white list" through.
     def item_params
-      params.require(:item).permit(:img_url, :img_url2, :img_url3, :img_url4, :img_url5, :title, :description, :state, :city, :location, :category)
+      # params.require(:item).permit(:title, :description, :state, :city, :location, :category, :user_id, :price)
+      params.permit(:title, :description, :state, :city, :location, :category, :user_id, :price, :id)
     end
+
+    def picture_params
+      params.permit(:picture, :id, :username)
+    end 
 end
