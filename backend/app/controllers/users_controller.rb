10b378ae0425b159adf8bfa6128a877266f3e856
08fdange <08fdange@gmail.com>
2020-11-29 16:48:07 -0500

    Updated user controller with avatar upload and attachment

--- backend/app/controllers/users_controller.rb
+++ backend/app/controllers/users_controller.rb
@@ -1,20 +1,26 @@
 class UsersController < ApplicationController
     before_action :authenticate_request, except: :create
-    before_action :find_user, except: %i[create index]
+    before_action :find_user, except: %i[create index upload_avatar]
 
     def index
-        @users = User.all   
-        render json: @users, status: :ok
+        @users = User.all
+        
+        users_json = UserSerializer.new(@users).serialized_json
+        render json: users_json, status: :ok
     end
 
     def show
-        render json: @user, status: :ok
+        render json: UserSerializer.new(@user).serialized_json, status: :ok
     end
 
     def create
         @user = User.new(user_params)
         if @user.save
-            render json: @user, status: :created
+            command = AuthenticateUser.call(@user.email, @user.password)
+
+            if command.success?
+                render json: {user: @user, auth_token: command.result}, status: :created
+            end
         else
             render json: { errors: @user.errors.full_messages }, status: :unprocessable_entity
         end
@@ -30,18 +36,34 @@ class UsersController < ApplicationController
         @user.destroy
     end
 
+    def upload_avatar
+        @user = User.find_by(username: avatar_params[:username])
+        @user.avatar.attach(avatar_params[:avatar])
+        if @user.avatar.attached?
+            avatar_serializer = AvatarSerializer.new(avatar: @user.avatar, user: @user)
+            render json: avatar_serializer.serialize_new_avatar()
+        else
+            render json: {errors: "No Avatar Attached"}, status: 400
+        end
+    end
+
     private
 
-    def find_user
-        @user = User.find_by_username!(params[:_username])
-        rescue ActiveRecord::RecordNotFound
-          render json: { errors: 'User not found' }, status: :not_found
-      end
+        def find_user
+            @user = User.find_by(username: params[:username])
+            rescue ActiveRecord::RecordNotFound
+            render json: { errors: 'User not found' }, status: :not_found
+        end
     
-      def user_params
-        params.permit(
-          :avatar, :name, :username, :email, :password, :password_confirmation
-        )
-      end
+        def user_params
+            params.require(:user).permit(
+                :name, :username, :email, :password, :password_confirmation, :phone_number
+            )
+        end
+
+        def avatar_params
+            params.permit(:avatar, :username)
+        end
+
 end
 
