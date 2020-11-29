cb639303e947c1e62f3a789f60ae5f59bda8df7b
08fdange <08fdange@gmail.com>
2020-11-29 16:29:13 -0500

    Added routes

--- backend/config/routes.rb
+++ backend/config/routes.rb
@@ -1,5 +1,14 @@
 Rails.application.routes.draw do
-  resources :users, param: :_username
-  resources :items
+  resources :users, param: :username do
+    member do
+      resources :items
+    end
+  end
+
+  resources :items, only: [:index, :show]
   post 'users/auth', to: 'authentication#authenticate'
+  post 'users/:username/upload_avatar', to: 'users#upload_avatar'
+  post 'users/:username/items/:id/upload_picture', to: 'items#upload_picture'
+  
+
 end
