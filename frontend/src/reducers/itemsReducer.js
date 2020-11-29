08f8903a8fd4b38cffbd414bfc7d74586c6e0905
08fdange <08fdange@gmail.com>
2020-11-29 17:21:37 -0500

    Updated reducers and working rootReducer in reducers/index.js

--- frontend/src/reducers/itemsReducer.js
+++ frontend/src/reducers/itemsReducer.js
@@ -1,19 +1,20 @@
-export default function itemsReducer(state = {items: [], loading: false}, action) {
+export default function itemsReducer(state = {items: [], newItem: "", loading: false}, action) {
     switch(action.type) {
         case "LOADING_ITEMS":
-           return{
-             ...state,
-            items: [...state.items],
-            loading: true
-           }
+            return{
+                ...state,
+                items: [...state.items],
+                loading: true
+            }
+
         case 'ADD_ITEMS':
             return {
                 ...state,
-                items: action.items,
+                items: action.payload,
                 loading: false
             }
+
         default:
             return state;
     }
 }
-
--- frontend/src/reducers/usersReducer.js
+++ frontend/src/reducers/usersReducer.js
@@ -1,15 +1,51 @@
 const initialState = {
-    currentUser: {},
-    users: []
+    loggedIn: false,
+    users: [],
+    user: {},
+    errors: [],
+    error: []
 }
 
-export default function usersReducer(state = initialState, action) {
+export default function userReducer(state = initialState, action) {
     switch (action.type) {
-        case 'LOGIN_USER':
-            return {...state, currentUser: action.payload}
+        case 'SET_ERRORS':
+            localStorage.clear()
+            return {
+                ...state,
+                loggedIn: false,
+                user: {},
+                errors: action.payload
+            }
+        
+        case 'SET_ERROR':
+            return {
+                ...state,
+                loggedIn: false,
+                user: {},
+                error: action.payload
+            }
+
+        case 'SET_USER':
+            return {
+                ...state,
+                loggedIn: true,
+                user: {...action.payload}
+            }
+        
+        case 'LOG_OUT':
+            localStorage.clear()
+            return {
+                ...state,
+                loggedIn: false,
+                user: {},
+                errors: []
+            }
         
         case 'ADD_USERS':
-            return {...state, users: action.users}
+            return {
+                ...state, 
+                users: action.payload
+            }
 
         default:
             return state;
