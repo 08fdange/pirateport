2f7f84458294f603bca6a50f41cb05ba410ef73b
08fdange <08fdange@gmail.com>
2020-11-29 16:51:32 -0500

    Updated user model with validations and avatar attachment

--- backend/app/models/user.rb
+++ backend/app/models/user.rb
@@ -1,5 +1,11 @@
 class User < ApplicationRecord
+    include Rails.application.routes.url_helpers
+
     has_secure_password
+    has_many :items
+    has_one_attached :avatar 
+    # default_url: "./assets/images/no-profile-photo.png"
+
     # mount_uploader :avatar, AvatarUploader
     validates :email, presence: true, uniqueness: true
     validates :email, format: { with: URI::MailTo::EMAIL_REGEXP }
@@ -7,4 +13,10 @@ class User < ApplicationRecord
     validates :password,
               length: { minimum: 6 },
               if: -> { new_record? || !password.nil? }
+
+    def get_avatar_url
+        if self.avatar.attached?
+            url_for(self.avatar)
+        end
+    end
 end
