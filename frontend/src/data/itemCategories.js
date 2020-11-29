26754d57c2b8bb044028ec5d3ad4ec67180977b1
08fdange <08fdange@gmail.com>
2020-11-29 17:36:36 -0500

    Added data and media files

---
+++ frontend/src/data/itemCategories.js
@@ -0,0 +1,25 @@
+const itemCategories = [
+	"Accessories",
+	"Appliances",
+	"Bikes",
+	"Books",
+	"Cars",
+	"Clothing",
+	"Computers",
+	"Electronics",
+	"Furniture",
+	"Jewelry",
+	"Miscellaneous",
+	"Motorcycles",
+	"Musical Instruments",
+	"Sporting Goods",
+	"Tickets",
+	"Televisions",
+	"Tools",
+	"Trailers",
+	"Video Games",
+	"Wheels/Tires"	
+	
+]
+
+export default itemCategories;
