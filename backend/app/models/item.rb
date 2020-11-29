69fe0a625145115aedd33893ed48cb8f6ebedf1f
08fdange <08fdange@gmail.com>
2020-11-29 16:52:45 -0500

    Item model added with validations and picture attachment

--- backend/app/models/item.rb
+++ backend/app/models/item.rb
@@ -1,2 +1,14 @@
 class Item < ApplicationRecord
+    include Rails.application.routes.url_helpers
+    belongs_to :user
+    has_one_attached :picture
+
+    validates :title, :category, :state, :city, :price, presence: true
+
+    def get_picture_url
+        if self.picture.attached?
+            url_for(self.picture)
+        end
+    end
+
 end
