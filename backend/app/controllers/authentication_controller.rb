8ff5910462dd017fd3fc8a797ba82122379271d2
08fdange <08fdange@gmail.com>
2020-11-29 16:46:04 -0500

    Changed authentication controller to send user info as well as jwt token in json form after auth

--- backend/app/controllers/authentication_controller.rb
+++ backend/app/controllers/authentication_controller.rb
@@ -4,7 +4,13 @@ class AuthenticationController < ApplicationController
         command = AuthenticateUser.call(params[:email], params[:password])
 
         if command.success?
-            render json: { auth_token: command.result }
+            render json: { auth_token: command.result, user: {
+                email: params[:email], 
+                name: User.find_by(email: params[:email]).name,
+                username: User.find_by(email: params[:email]).username,
+                user_id: User.find_by(email: params[:email]).id
+                }}
+
         else
             render json: { error: command.errors }, status: :unauthorized
         end
